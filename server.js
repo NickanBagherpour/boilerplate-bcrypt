6ffbe94dc1854ccab5420db91cd745f58fa73071
ni.bagherpour <ni.bagherpour@sadad.co.ir>
2023-01-28 21:13:17 +0330

    feat: hash and compare passwords synchronously

--- server.js
+++ server.js
@@ -1,44 +1,32 @@
-'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const bcrypt  = require('bcrypt');
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const bcrypt = require("bcrypt");
 
-const app         = express();
+const app = express();
 fccTesting(app);
 const saltRounds = 12;
-const myPlaintextPassword = 'sUperpassw0rd!';
-const someOtherPlaintextPassword = 'pass123';
-
+const myPlaintextPassword = "sUperpassw0rd!";
+const someOtherPlaintextPassword = "pass123";
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-    console.log(hash);
+  console.log(hash);
 
-    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-      console.log(res); 
-    });
-
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
   });
+});
 
 //END_ASYNC
 
 //START_SYNC
 
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
 
 //END_SYNC
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(process.env.PORT || 3000, () => {});
